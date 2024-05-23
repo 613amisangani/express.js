11eecaee08c027f0a1f25b0bc2d83ab19367baa1
613amisangani <amisangani7@gmail.com>
2024-05-23 09:14:30 +0530

    crud

--- base.js
+++ base.js
@@ -9,31 +9,72 @@ const products = require('./public/data.json');
 console.log(products);
 
 
- server.use(express.json());  //for the json data
+server.use(express.json());  //for the json data
 server.use(morgan('dev'))
 
 
 //crud 
 
-server.post("/products",(req,res)=>{  //for json data add
+//for json data add
+
+server.post("/products", (req, res) => {  
     products.push(req.body)
-    res.status(201).json({message:'new product is addedd.....'})
+    res.status(201).json({ message: 'new product is addedd.....' })
 })
 
-      server.get("/products",(req,res)=>{  //for data read
-        res.status(200).json(products);
-     })
+//for data read
 
-     server.get("/products/:id",(req,res)=>{  //for read specific data
+server.get("/products", (req, res) => {  
+    res.status(200).json(products);
+})
+
+//for read specific data
+
+server.get("/products/:id", (req, res) => {  
     // console.log(typeof(id))
-        const id = +req.params.id;
-        const item = products.find((e)=>e.id === id) 
-        res.status(200).json(item);
-     })
+    const id = +req.params.id;
+    const item = products.find((e) => e.id === id)
+    res.status(200).json(item);
+})
+
+//for data replace
+
+server.put("/products/:id", (req, res) => {  
+    // console.log(typeof(id))
+    const id = +req.params.id;
+    const itemindex = products.findIndex((e) => e.id === id)
+    // const product = products[itemindex]
+    products.splice(itemindex,1,{...req.body})
+    res.status(200).json({message:'product is replaced....'});
+})
+
+
+//for data update
+
+server.patch("/products/:id", (req, res) => {  
+    // console.log(typeof(id))
+    const id = +req.params.id;
+    const itemindex = products.findIndex((e) => e.id === id)
+     const product = products[itemindex]
+    products.splice(itemindex,1,{...product,...req.body})
+    res.status(200).json({message:'product is updated....'});
+})
+
+//for data delete
+
+server.delete("/products/:id", (req, res) => {  
+    // console.log(typeof(id))
+    const id = +req.params.id;
+    const itemindex = products.findIndex((e) => e.id === id)
+    //  const product = products[itemindex]
+    products.splice(itemindex,1)
+    res.status(200).json({message:'product is deleted....'});
+})
 
 
 
-server.listen(1122,() =>{
+
+server.listen(1122, () => {
     console.log('server is created.....')
 })
 
